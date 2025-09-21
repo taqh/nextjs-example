21f660973a123fa6ff958dc16d6bba62f6722056
Taqib <taqib.ibrahim@gmail.com>
2025-09-21 23:08:05 +0100

    chore: analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Geist_Mono, Literata, Manrope } from 'next/font/google';
 import { ViewTransitions } from 'next-view-transitions';
 import { SITE } from '@/lib/site';
+import { Analytics } from "@vercel/analytics/next";
 
 const manrope = Manrope({
   variable: '--font-sans',
@@ -36,6 +37,7 @@ export default function RootLayout({
           className={`${manrope.variable} ${geistMono.variable} ${literata.variable} antialiased`}
         >
           {children}
+          <Analytics />
         </body>
       </html>
     </ViewTransitions>
